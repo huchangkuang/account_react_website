a679e599d166c134dc058cf620691f89b1373ab7
huchangkuang <603021137@qq.com>
2020-08-24 15:07:51 +0800

    deploy

---
+++ precache-manifest.da36408d8667bd1632b13f51ed73d025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fc0bb8f8fa9ec9b29a1cf25c68a718",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7b10c052fae4bdab530",
+    "url": "./static/css/main.2de7e335.chunk.css"
+  },
+  {
+    "revision": "28856b1857b4069d90fd",
+    "url": "./static/js/2.042cf3a4.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "./static/js/2.042cf3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b10c052fae4bdab530",
+    "url": "./static/js/main.f48ba3b0.chunk.js"
+  },
+  {
+    "revision": "22d00c35fb3db5a406d2",
+    "url": "./static/js/runtime-main.f9a680f2.js"
+  }
+]);
